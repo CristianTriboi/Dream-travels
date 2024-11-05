52d73216e6460b6c6405d83c906b10c7a2aabc5e
Cristian <cristian.triboi@gmail.com>
2024-11-05 07:09:05 -0500

    modify the logic of js for fetch data

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -5,26 +5,32 @@ const btnReset = document.getElementById('btnReset');
 function searchDestination() {
     const input = document.getElementById('destinationInput').value.toLowerCase();
     const resultDiv = document.getElementById('result');
+    console.log(input);
     resultDiv.innerHTML = '';
-    fetch('health_analysis.json')
+    fetch('travel_recommendation_api.json')
         .then(response => response.json())
         .then(data => {
-        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
-        if (condition) {
-            const symptoms = condition.symptoms.join(', ');
-            const prevention = condition.prevention.join(', ');
-            const treatment = condition.treatment;
-            resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-            resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+
+        const destination = data.conditions.find(item => item.name.toLowerCase() === input);
+        console.log(response);
+        console.log(destination);
+        /*if (destination) {
+            const symptoms = destination.symptoms.join(', ');
+            const prevention = destination.prevention.join(', ');
+            const treatment = destination.treatment;
+            resultDiv.innerHTML += `<h2>${destination.name}</h2>`;
+            resultDiv.innerHTML += `<img src="${destination.imagesrc}" alt="hjh">`;
             resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
             resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
             resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
         } else {
-            resultDiv.innerHTML = 'Condition not found.';
-        }
+            resultDiv.innerHTML = 'Destination not found.';
+        }*/
         })
         .catch(error => {
         console.error('Error:', error);
         resultDiv.innerHTML = 'An error occurred while fetching data.';
         });
 }
+
+btnSearch.addEventListener('click', searchDestination);
