b8d4304bf31b4dc68a40b711cf07c918ba79046e
cristian <cristian.triboi@gmail.com>
2024-11-08 19:19:44 -0500

    add a clean result method

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -7,33 +7,25 @@ resultDiv.innerHTML = '';
 function searchDestination() {
     const input = document.getElementById('destinationInput').value.toLowerCase();
     const resultDiv = document.getElementById('result');
-    console.log(input);
     resultDiv.innerHTML = '';
     fetch('travel_recommendation_api.json')
         .then(response => response.json())
         .then(data => {
 
         const keyMatch = containsWord(input, Object.keys(data));
-        /*console.log(keyMatch);
-        console.log("single obj: " + JSON.stringify(data.countries));
-        
-        console.log(keyMatch);*/
+
         if (keyMatch) {
             for (const places of data?.[keyMatch]) {
                 const countryName = JSON.stringify(places.name);
-                //console.log("try to iterate through array: " + JSON.stringify(places));
                 if ("cities" in places) {
                     for(const citiesObj of places?.["cities"]) {
                         if (!citiesObj.description) {
                             continue;
                         }
                         buildHtml(citiesObj);
-                        //console.log("we found the cities object: " + JSON.stringify(citiesObj));
                     }
                 }
                 buildHtml(places);
-                /*const cities = places.name;
-                console.log(cities);*/
             }
         } else {
             resultDiv.innerHTML = 'Destination not found.';
@@ -60,7 +52,7 @@ function buildHtml(finalObj) {
     console.log("we finaly are here with the final obj " + finalObj.name);
     /*const name = finalObj.name.join(', ');
     const description = finalObj.description;?*/
-    resultDiv.innerHTML += `<div class="card">`;
+    resultDiv.innerHTML += `<div id='card' class="card">`;
     resultDiv.innerHTML += `<h2>${finalObj.name}</h2>`;
     resultDiv.innerHTML += `<img src="${finalObj.imageUrl}" alt="card-image" class="card-image">`;
     resultDiv.innerHTML += `<div class="card-content">`;
@@ -70,6 +62,9 @@ function buildHtml(finalObj) {
     resultDiv.innerHTML += `</div>`;
 }
 
-
+function resetResults() {
+    document.getElementById("result").innerHTML = "";
+}
 
 btnSearch.addEventListener('click', searchDestination);
+btnReset.addEventListener('click', resetResults);
